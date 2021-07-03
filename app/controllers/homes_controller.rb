b2a4f0495ee72971bf1bd6ff586cafa92b79ebe4
naonao-program <gf19a012@oecu.jp>
2021-07-03 13:05:17 +0900

    deviseの導入

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,13 @@
+class HomesController < ApplicationController
+  def top
+  end
+
+  def about
+  end
+
+  def app
+  end
+
+  def premium
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  get 'home/about', to: "homes#about"
+  get 'home/app', to: "homes#app"
+  get 'home/premium', to: "homes#premium"
+  resources :todo, only:[:index, :create,:show,:edit,:update]
 end
